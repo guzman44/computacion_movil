4ead22e724e4fc55e6a1f3eb6a304de7bed845a1
4Khaos <imroocker@gmail.com>
2020-03-15 23:30:18 -0500

    Transicion entre login y crearUsuario

--- Implementacion/CodigoFuente/Proyecto/app/src/main/java/co/movil/computacion/ui/login/LoginActivity.java
+++ Implementacion/CodigoFuente/Proyecto/app/src/main/java/co/movil/computacion/ui/login/LoginActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -23,6 +24,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import co.movil.computacion.R;
+import co.movil.computacion.ui.NewUser;
 import co.movil.computacion.ui.login.LoginViewModel;
 import co.movil.computacion.ui.login.LoginViewModelFactory;
 
@@ -39,6 +41,7 @@ public class LoginActivity extends AppCompatActivity {
 
         final EditText usernameEditText = findViewById(R.id.username);
         final EditText passwordEditText = findViewById(R.id.password);
+        final TextView register = findViewById(R.id.registrar);
         final Button loginButton = findViewById(R.id.login);
         final ProgressBar loadingProgressBar = findViewById(R.id.loading);
 
@@ -115,6 +118,14 @@ public class LoginActivity extends AppCompatActivity {
                 loadingProgressBar.setVisibility(View.VISIBLE);
                 loginViewModel.login(usernameEditText.getText().toString(),
                         passwordEditText.getText().toString());
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newUserIntent = new Intent( v.getContext(), NewUser.class);
+                startActivity( newUserIntent );
             }
         });
     }
