729c5a7109b36b0db4e3d75f5204ca2bb3c52def
4Khaos <imroocker@gmail.com>
2020-03-22 01:06:01 -0500

    Controlador evento

--- Implementacion/CodigoFuente/Proyecto/app/src/main/java/co/movil/computacion/controller/Event.java
+++ Implementacion/CodigoFuente/Proyecto/app/src/main/java/co/movil/computacion/controller/Event.java
@@ -4,9 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import co.movil.computacion.R;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
 
 public class Event extends AppCompatActivity {
+
+    ImageView targetImage;
+    Button buttonLoadImage;
+    Button buttonSave;
+    String etUser;
+    String etName;
+    String etLastName;
+    String etEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +39,52 @@ public class Event extends AppCompatActivity {
             fragmentDemo.activity(optionMenu);
         }
 
+        buttonSave = (Button)findViewById(R.id.btnSave);
+        buttonLoadImage = (Button)findViewById(R.id.btnLoadImage);
+        targetImage = (ImageView)findViewById(R.id.ivuserprofile);
+        etUser = ((EditText)findViewById(R.id.etUser)).toString();
+        etName = ((EditText)findViewById(R.id.etName)).toString();
+        etLastName = ((EditText)findViewById(R.id.etLastName)).toString();
+        etEmail = ((EditText)findViewById(R.id.etEmail)).toString();
+
+        buttonLoadImage.setOnClickListener(new Button.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 0);
+            }});
+
+        buttonSave.setOnClickListener( new Button.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( v.getContext(), Home.class );
+                Bundle bundle = new Bundle();
+                bundle.putString( "usuario", etUser );
+                bundle.putString( "nombre", etName );
+                bundle.putString( "apellido", etLastName );
+                bundle.putString( "email", etEmail );
+                intent.putExtra( "info", bundle );
+                startActivity( intent );
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK){
+            Uri targetUri = data.getData();
+            Bitmap bitmap;
+            try {
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+                targetImage.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- Implementacion/CodigoFuente/Proyecto/app/src/main/java/co/movil/computacion/controller/Profile.java
+++ Implementacion/CodigoFuente/Proyecto/app/src/main/java/co/movil/computacion/controller/Profile.java
@@ -4,26 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import co.movil.computacion.R;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.FileNotFoundException;
 
 public class Profile extends AppCompatActivity {
-
-
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
-
     }
-
-
-
-
 }
