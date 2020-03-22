35176b4f5418e0517afb4b3a9042a52a422f382f
Marco Portatil <mguzmanmartinez@gmail.com>
2020-03-21 23:01:01 -0500

    Se agregan la pantalla de multimedia duncional

--- Implementacion/CodigoFuente/Proyecto/app/src/main/java/co/movil/computacion/controller/Multimedia.java
+++ Implementacion/CodigoFuente/Proyecto/app/src/main/java/co/movil/computacion/controller/Multimedia.java
@@ -1,36 +1,19 @@
 package co.movil.computacion.controller;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
+
 import co.movil.computacion.R;
 
-import android.Manifest;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.MediaScannerConnection;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import java.io.File;
-
 public class Multimedia extends AppCompatActivity {
-
-    final int COD_SELECCIONA=10;
-    private final String CARPETA_RAIZ="misImagenesPrueba/";
-    private final String RUTA_IMAGEN=CARPETA_RAIZ+"misFotos";
-    final int COD_FOTO=20;
-    String path;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     ImageView imagen;
     Button btnGrid;
 
@@ -51,104 +34,23 @@ public class Multimedia extends AppCompatActivity {
             fragmentDemo.activity(optionMenu);
         }
 
-        if(validaPermisos()){
-            btnGrid.setEnabled(true);
-        }else{
-            btnGrid.setEnabled(false);
-        }
+        btnGrid.setEnabled(true);
     }
 
     public void onClick(View view) {
-        tomarFotografia();
-        /*Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI );
-        intent.setType("image/*");
-        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-        startActivityForResult(intent.createChooser(intent,"Seleccione la Aplicación"),COD_SELECCIONA);*/
-
-    }
-
-    private void tomarFotografia() {
-        File fileImagen=new File(Environment.getExternalStorageDirectory(),RUTA_IMAGEN);
-        boolean isCreada=fileImagen.exists();
-        String nombreImagen="";
-        if(isCreada==false){
-            isCreada=fileImagen.mkdirs();
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
-
-        if(isCreada==true){
-            nombreImagen=(System.currentTimeMillis()/1000)+".jpg";
-        }
-
-
-        path=Environment.getExternalStorageDirectory()+
-                File.separator+RUTA_IMAGEN+File.separator+nombreImagen;
-
-        File imagen=new File(path);
-
-        Intent intent=null;
-        intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        ////
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.N)
-        {
-            String authorities=getApplicationContext().getPackageName()+".provider";
-            Uri imageUri= FileProvider.getUriForFile(this,authorities,imagen);
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-        }else
-        {
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(imagen));
-        }
-        startActivityForResult(intent,COD_FOTO);
-    }
-
-    private boolean validaPermisos() {
-
-        if(Build.VERSION.SDK_INT<Build.VERSION_CODES.M){
-            return true;
-        }
-
-        if((checkSelfPermission(CAMERA_SERVICE)== PackageManager.PERMISSION_GRANTED)&&
-                (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED)){
-            return true;
-        }
-
-        if((shouldShowRequestPermissionRationale(CAMERA_SERVICE)) ||
-                (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE))){
-            cargarDialogoRecomendacion();
-        }else{
-            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,CAMERA_SERVICE},100);
-        }
-
-        return false;
-    }
-
-    private void cargarDialogoRecomendacion() {
-        AlertDialog.Builder dialogo=new AlertDialog.Builder(Multimedia.this);
-        dialogo.setTitle("Permisos Desactivados");
-        dialogo.setMessage("Debe aceptar los permisos para el correcto funcionamiento de la App");
-
-        dialogo.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,CAMERA_SERVICE},100);
-            }
-        });
-        dialogo.show();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode==RESULT_OK){
-            MediaScannerConnection.scanFile(this, new String[]{path}, null,
-                    new MediaScannerConnection.OnScanCompletedListener() {
-                        @Override
-                        public void onScanCompleted(String path, Uri uri) {
-                            Log.i("Ruta de almacenamiento","Path: "+path);
-                        }
-                    });
-
-            Bitmap bitmap= BitmapFactory.decodeFile(path);
-            imagen.setImageBitmap(bitmap);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imagen.setImageBitmap(imageBitmap);
         }
     }
+
 }
--- Implementacion/CodigoFuente/Proyecto/app/src/main/java/co/movil/computacion/model/FooterItem.java
+++ Implementacion/CodigoFuente/Proyecto/app/src/main/java/co/movil/computacion/model/FooterItem.java
@@ -14,6 +14,7 @@ public class FooterItem extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.footer_layout, container, false);
+        //return inflater.inflate(R.layout.footer_layout, container, false);
+        return null;
     }
 }
