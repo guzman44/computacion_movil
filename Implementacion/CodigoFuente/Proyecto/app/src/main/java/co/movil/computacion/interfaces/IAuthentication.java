87692be4fa3c6fefdd85ef0691b59074c9ce9973
Marco Portatil <mguzmanmartinez@gmail.com>
2020-05-24 09:59:53 -0500

    Se colocan los servicios del componente de swqgger de sutenticacion

--- Implementacion/CodigoFuente/Proyecto/app/src/main/java/co/movil/computacion/interfaces/IAuthentication.java
+++ Implementacion/CodigoFuente/Proyecto/app/src/main/java/co/movil/computacion/interfaces/IAuthentication.java
@@ -1,11 +1,16 @@
 package co.movil.computacion.interfaces;
 
+import co.movil.computacion.dtos.Account.AcountDTO;
+import co.movil.computacion.dtos.Account.PasswordDTO;
+import co.movil.computacion.dtos.Account.PerfilUserDTO;
+import co.movil.computacion.dtos.ResponseDTO;
 import co.movil.computacion.model.RequestAuthentication;
 import co.movil.computacion.model.UserTokenViewModel;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 
 public interface IAuthentication {
 
@@ -15,6 +20,27 @@ public interface IAuthentication {
    Call<UserTokenViewModel> login(
            @Header("Content-Type") String content_type,
            @Body RequestAuthentication body
+    );
+
+
+    @POST("api/Authentication/create/acount")
+    Call<ResponseDTO> crarCuenta(
+            @Header("Content-Type") String content_type,
+            @Body AcountDTO body
+    );
+
+
+    @POST("api/Authentication/change/password")
+    Call<ResponseDTO> cambiarPassword(
+            @Header("Content-Type") String content_type,
+            @Body PasswordDTO body
+    );
+
+
+    @PUT("api/Authentication/update/perfilUser")
+    Call<ResponseDTO> actualizarPerfil(
+            @Header("Content-Type") String content_type,
+            @Body PerfilUserDTO body
     );
 
 
